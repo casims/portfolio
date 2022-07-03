c6e6352b154dd0918d202f78d5fdfface6e2cc28
Connor <csims9@hotmail.com>
2022-07-03 08:38:19 -0700

    Add and fix HTML outputs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 const port = {
     target: document.querySelector('main#main'),
     jsonURLMain:'https://casims.ca/csport/wp-json/wp/v2/csp-project?_embed',
-    contentHeader: `
-    <h1 id="main-title">Connor Sims</h1>
-    <p>Entry-level Front-End Engineer with a passion for JavaScript and problem solving.</p>
-    `,
+    contentHTML: '',
     // getJSON: function(url) {
     //     fetch(url)
     //         .then(response => response.json())
@@ -20,17 +17,40 @@ const port = {
     outputContentMain: async function() {
         await this.getJSON(this.jsonURLMain);
         console.log(port.jsonData);
-        this.target.innerHTML = this.contentHeader;
+        this.contentHTML += `
+            <section id="landing-section">
+                <h1 id="main-title">Connor Sims</h1>
+                <p>Entry-level Front-End Engineer with a passion for JavaScript and problem solving.</p>
+            </section>
+            <section id="projects-section">
+                <h2>Projects</h2>`;
         this.outputContentProjects();
+        this.contentHTML += `
+            </section>
+            <section id="contact-section">
+
+            </section>`;
+        this.target.innerHTML = this.contentHTML;
     },
     outputContentProjects: function() {
         for (let project of port.jsonData) {
-            this.target.innerHTML += `
-            <article class="project-card">
-                <img src="${project['_embedded']['wp:featuredmedia'][0].media_details.sizes.medium.source_url}" alt="${project['_embedded']['wp:featuredmedia'][0].alt_text}">
-                <h3>${project.title.rendered}</h3>
-            </article>
-        `
+            this.contentHTML += `
+                <article class="project-card">
+                    <img src="${project['_embedded']['wp:featuredmedia'][0].media_details.sizes.medium.source_url}" alt="${project['_embedded']['wp:featuredmedia'][0].alt_text}">
+                    <h3>${project.title.rendered}</h3>
+                    <h4>${project.acf.proj_sub_title}</h4>
+                    <ul>`;
+            let terms = project['_embedded']['wp:term'][0];
+            for (let term of terms) {
+            this.contentHTML += `<li>${term.name}</li>`;
+            };
+            this.contentHTML += `
+                    </ul>
+                    <p>${project.acf.proj_duration}</p>
+                    <p>${project.acf.proj_overview}</p>
+                    <a href="#${project.id}">Project Details</a>
+                </article>
+            `;
         }
     }
 };
