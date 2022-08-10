5e48e7d23e7c95c5a3229faae89e1b92fcc90695
Connor <csims9@hotmail.com>
2022-08-09 23:13:25 -0700

    Add accordian functionality

--- js/script.js
+++ js/script.js
@@ -44,6 +44,10 @@ const port = {
                 <path d="M0 0v24h24v-24h-24zm14.534 19.59c-.406.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.311-1.588-.824-2.147.083-.202.357-1.016-.079-2.117 0 0-.671-.215-2.198.82-.639-.18-1.323-.267-2.003-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/>
             </svg>
         </a>`,
+    arrowSVG: `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M12 21l-12-18h24z"/>
+        </svg>`,
     contentContactSection: `
         <section id="contact">
             <h2 tabindex="0">Contact</h2>
@@ -194,11 +198,6 @@ const port = {
                 toolOutput.innerHTML = '';
             };
         });
-        // document.addEventListener('mouseleave', (event) => {
-        //     if (event.target.classList.contains('svg-hover')) {
-        //         toolOutput.innerHTML = '';
-        //     };
-        // });
     },
     outputContentProject: async function() {
         this.target.innerHTML = '';
@@ -244,10 +243,11 @@ const port = {
             let sections = port.jsonData.acf.proj_section_gen;
             console.log(sections);
             if (Array.isArray(sections)) {
-                this.contentHTML += `<section id="project-sect-section">`;
+                this.contentHTML += `<section id="project-sect-section" class="accord">`;
                 for (let section of sections) {
                     this.contentHTML += `
-                            <h3 tabindex="0">${section.proj_sect_gen_heading}</h3>
+                            <h3 tabindex="0" class="accord-button">${section.proj_sect_gen_heading}</h3>
+                            ${this.arrowSVG}
                             <p>${section.proj_sect_gen_text}</p>`;
                         if (section.proj_sect_gen_image) {
                             let imageID = section.proj_sect_gen_image;
@@ -260,13 +260,14 @@ const port = {
                 this.contentHTML += `</section>`;
             }
             this.contentHTML += `
-                <section id="project-feat-section">
-                    <h2 tabindex="0">Features</h2>
+                <section id="project-feat-section" class="accord">
+                    <h3 tabindex="0" class="accord-button">Features</h3>
+                    ${this.arrowSVG}
                     <p>${port.jsonData.acf.proj_features_intro}</p>`;
             let features = port.jsonData.acf.proj_features_gen;
             for (let feature of features) {
                 this.contentHTML += `
-                    <h3 tabindex="0">${feature.proj_feat_gen_heading}</h3>
+                    <h4 tabindex="0">${feature.proj_feat_gen_heading}</h4>
                     <p>${feature.proj_feat_gen_text}</p>
                     ${feature.proj_feat_gen_code}`;
                 if (feature.proj_feat_gen_image) {
@@ -281,8 +282,9 @@ const port = {
             };
             this.contentHTML += `
                 </section>
-                <section id="project-skills-section">
-                    <h2 tabindex="0">Gained Skills</h2>
+                <section id="project-skills-section" class="accord">
+                    <h3 tabindex="0" class="accord-button">Takeaways</h3>
+                    ${this.arrowSVG}
                     <ul>`;
             let skills = port.jsonData.acf.proj_skills_list;
             for (let skill of skills) {
@@ -306,7 +308,26 @@ const port = {
             this.contentHTML += `</section>`;
             loading.style.display = 'none';
             this.target.innerHTML = this.contentHTML;
+            this.accordListeners();
         };
+    },
+    accordListeners: function() {
+        let accordSections = document.getElementsByClassName('accord');
+        console.log(accordSections);
+        document.addEventListener('click', (event) => {
+            if (event.target.classList.contains('accord-button')) {
+                if (event.target.parentElement.classList.contains('expanded')) {
+                    for (let accordSection of accordSections) {
+                        accordSection.classList.remove('expanded');
+                    };
+                } else {
+                    for (let accordSection of accordSections) {
+                        accordSection.classList.remove('expanded');
+                    };
+                    event.target.parentElement.classList.add('expanded');
+                };
+            };
+        });
     },
 };
 
