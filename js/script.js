0075838226b89c303c5d4ae52e9f9b51ed1c6d73
Connor <csims9@hotmail.com>
2024-01-13 14:27:32 -0800

    Add styles for back button

--- js/script.js
+++ js/script.js
@@ -411,7 +411,11 @@ const port = {
             this.contentHTML = `
                 <nav id="site-navigation">
                     <div id="nav-button-container">
-                        <a id="back-button" href="">Back</a>
+                        <a id="back-button" href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path d="M0 3.795l2.995-2.98 11.132 11.185-11.132 11.186-2.995-2.981 8.167-8.205-8.167-8.205zm18.04 8.205l-8.167 8.205 2.995 2.98 11.132-11.185-11.132-11.186-2.995 2.98 8.167 8.206z"/>
+                            </svg>
+                        </a>
                     </div>
                 </nav>
                 <h2>404 Error</h2>
@@ -422,7 +426,11 @@ const port = {
                 <a class="screen-reader-text" href="#project-header-section">Skip to content</a>
                 <nav id="site-navigation">
                     <div id="nav-button-container">
-                        <a id="back-button" href="">Back</a>
+                        <a id="back-button" href="" class="circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path d="M0 3.795l2.995-2.98 11.132 11.185-11.132 11.186-2.995-2.981 8.167-8.205-8.167-8.205zm18.04 8.205l-8.167 8.205 2.995 2.98 11.132-11.185-11.132-11.186-2.995 2.98 8.167 8.206z"/>
+                            </svg>
+                        </a>
                     </div>
                 </nav>
                 <section id="proj-landing">
