218d40c686d5ce1f5db13ca73c3f6e68ee37b65b
Connor <csims9@hotmail.com>
2024-01-15 12:54:17 -0800

    Add scroll button to land sect for larger screens

--- js/script.js
+++ js/script.js
@@ -253,6 +253,13 @@ const port = {
         this.outputContentTools();
         this.contentHTML += `
                 </div>
+                <div class="scroll-button-wrapper">
+                    <a href="#projects" class="circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/>
+                        </svg>
+                    </a>
+                </div>
             </section>
             <section id="projects">
                 <div class="card header">
