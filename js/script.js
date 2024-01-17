35af24b50443d862c259ecdfd7e37ac0d4cbee45
Connor <csims9@hotmail.com>
2024-01-17 11:11:11 -0800

    Centered content

--- js/script.js
+++ js/script.js
@@ -661,7 +661,6 @@ const port = {
         // setTimeout(() => {
             modalTarget.className = 'hidden';
             port.body.style.overflow = 'visible';
-            port.target.style.filter = 'none';
             setTimeout(() => {
                 modalTarget.style.display = 'none';
             }, 150);
@@ -670,7 +669,6 @@ const port = {
     modalShow: function(modalTarget) {
         modalTarget.style.display = 'flex';
         port.body.style.overflow = 'hidden';
-        port.target.style.filter = 'blur(6px)';
         setTimeout(() => {
             modalTarget.className = '';
         }, 1);
